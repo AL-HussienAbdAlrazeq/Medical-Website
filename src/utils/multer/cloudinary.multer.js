fd348a573400e51eb02d3c4ad0ee23062d9d7681
hussein <hussein@gmail.com>
2025-02-09 20:47:10 +0200

    second

--- src/utils/multer/cloudinary.multer.js
+++ src/utils/multer/cloudinary.multer.js
@@ -3,11 +3,10 @@ import path from "node:path";
 import * as dotenv from "dotenv";
 dotenv.config({ path: path.resolve("./src/config/.env") });
 import cloudinary from "cloudinary"
-
 cloudinary.v2.config({
-    cloud_name:"dakpezmhz",
-    api_key:"272798734248383",
-    api_secret:"BoEO1pvnZXTmxin7krq1gsO27wY",
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
     secure: true
 })
 
