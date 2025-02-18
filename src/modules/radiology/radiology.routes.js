e5341fa9ec366a9cb92064592418ff053b2360ea
hussein <hussein@gmail.com>
2025-02-18 18:38:58 +0200

    secondCommit

--- src/modules/radiology/radiology.routes.js
+++ src/modules/radiology/radiology.routes.js
@@ -7,12 +7,12 @@ import { createRadiologyValidation, updateRadiologyValidation } from "./radiolog
 
 const radiologyRouter = Router()
 
-radiologyRouter.post('/create-radiology', uploadCloudFile(fileTypes.allowedAttachments).array('images',2),validation(createRadiologyValidation), createRadiology)
+radiologyRouter.post('/create-radiology', uploadCloudFile(fileTypes.image).array('images',2),validation(createRadiologyValidation), createRadiology)
 // radiologyRouter.post('/create-radiology', uploadCloudFile(fileTypes.allowedAttachments).single('image'),validation(createRadiologyValidation), createRadiology)
 
 radiologyRouter.get('/', findAllRadiology)
 radiologyRouter.get('/:id', findRadiologyByID)
-radiologyRouter.patch('/update-citizen/:id', uploadCloudFile(fileTypes.allowedAttachments).array('images',2),validation(updateRadiologyValidation), updateRadiology)
+radiologyRouter.patch('/update-citizen/:id', uploadCloudFile(fileTypes.image).array('images',2),validation(updateRadiologyValidation), updateRadiology)
 radiologyRouter.delete('/delete-citizen/:id', deleteRadiology)
 
 
