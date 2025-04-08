da88eb842cebf4ebb2dca8c84518c05d1104b639
hussein <hussein@gmail.com>
2025-04-08 21:34:17 +0200

    commit35

--- src/Database/models/citizen.model.js
+++ src/Database/models/citizen.model.js
@@ -25,7 +25,7 @@ const citizenSchema = new mongoose.Schema(
     email: {
       type: String,
       // required: true,
-      unique: true
+      // unique: true
     },
     password: {
       type: String,
